d0b512161eb58f9bae4a39973fff4a0d2e626203
maurits111 <mauritspeelen@gmail.com>
2026-08-05 20:57:34 +0200

    Analytics toegevoegd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Waar kan ik het kijken?",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="nl" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
