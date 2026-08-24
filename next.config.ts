4c597faa4e4c22e2d3ab82759d589bdc9b56ef67
maurits111 <mauritspeelen@gmail.com>
2026-08-24 17:47:46 +0200

    Voeg eigen favicon toe

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: ".",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+    ],
   },
 };
 
